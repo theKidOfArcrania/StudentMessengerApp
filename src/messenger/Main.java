31f6182b2b2ece0c823d3abd4ee5b4c1248cce1c
Caleb Hoff <crunchycat1@gmail.com>
2015-11-05 13:25:18 -0600

    usernames must be at least 2 characters

--- src/messenger/Main.java
+++ src/messenger/Main.java
@@ -27,16 +27,16 @@ public class Main {
 		}
 		
 		String userName = "";
-		while (userName.isEmpty() | userName.length() > 30) {
+		while (userName.length() > 30| userName.length() < 2) {
 			userName = JOptionPane.showInputDialog(null, "Input your Username");
 			if (userName == null) {
 				return;	
 			}
-			if (userName.isEmpty()) {
-				showMessageDialog(null, "Invalid Username", "Messenger", WARNING_MESSAGE);
-			}
 			if (userName.length() > 30) {
 				showMessageDialog(null, "Invalid Username. The maximum is 30 Characters.", "Messenger", WARNING_MESSAGE);
+			}
+			if (userName.length() < 2) {
+				showMessageDialog(null, "Invalid Username. Username is too short.", "Messenger", WARNING_MESSAGE);
 			}
 			if (userName.equalsIgnoreCase("crunchycat")) {
 				showMessageDialog(null, "Reserved for Developer", "Messenger", WARNING_MESSAGE);
