3dac35364045c814d631fd0f58ba5a14a5eda0cb
theKidOfArcrania <wanghenry1998@gmail.com>
2015-11-07 17:33:31 -0600

    Flexible path testing

--- src/messenger/DefPath.java
+++ src/messenger/DefPath.java
@@ -5,10 +5,10 @@ import java.nio.file.Paths;
 
 public class DefPath {
 
-	public static Path DEF_MAIN_ROOT = Paths.get("S:/Templates/Review/StudentMessengerApp");
+	// public static Path DEF_MAIN_ROOT = Paths.get("S:/Templates/Review/StudentMessengerApp");
 
 	// For Testing:
-	//public static Path DEF_MAIN_ROOT = Paths.get("C:/Users/Caleb/Desktop/messenger/StudentMessengerApp"); //Please keep this line, henry... Testing in this location is important
-	//public static final Path DEF_MAIN_ROOT = Paths.get(System.getProperty("user.home"), "StudentMessengerApp");
+	// public static Path DEF_MAIN_ROOT = Paths.get("C:/Users/Caleb/Desktop/messenger/StudentMessengerApp");
+	public static final Path DEF_MAIN_ROOT = Paths.get(System.getProperty("user.home"), "Desktop", "messenger", "StudentMessengerApp"); // If you want to play that way, this testing is more generic and flexible to your computer. And school computer.
 
 }
