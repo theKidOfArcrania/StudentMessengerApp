64c6c873d2b43b9fb28f45048b501f8d45af8f60
Caleb Hoff <crunchycat1@gmail.com>
2015-11-09 10:09:39 -0600

    DefPath changed for school again.

--- src/messenger/DefPath.java
+++ src/messenger/DefPath.java
@@ -5,10 +5,8 @@ import java.nio.file.Paths;
 
 public class DefPath {
 
-	// public static Path DEF_MAIN_ROOT = Paths.get("S:/Templates/Review/StudentMessengerApp");
+	public static Path DEF_MAIN_ROOT = Paths.get("S:/Templates/Review/StudentMessengerApp");
 
-	// For Testing:
-	// public static Path DEF_MAIN_ROOT = Paths.get("C:/Users/Caleb/Desktop/messenger/StudentMessengerApp");
-	public static final Path DEF_MAIN_ROOT = Paths.get(System.getProperty("user.home"), "Desktop", "messenger", "StudentMessengerApp"); // If you want to play that way, this testing is more generic and flexible to your computer. And school computer.
+	//public static final Path DEF_MAIN_ROOT = Paths.get(System.getProperty("user.home"), "Desktop", "messenger", "StudentMessengerApp"); //For Testing
 
 }
